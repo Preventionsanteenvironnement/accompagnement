431f67b39452d42b159ca0f29beec5c28c68796b
Preventionsanteenvironnement <pseaclyon@gmail.com>
2026-01-31 11:14:36 +0100

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,10 @@
-// Firebase configuration - projet devoirs-pse
+// Configuration Firebase – projet devoirs-pse
 const firebaseConfig = {
-  apiKey: "TA_API_KEY",
+  apiKey: "AIzaSyAWdCMvOiAJln3eT9LIAQD3RWJUD0IQcLI",
   authDomain: "devoirs-pse.firebaseapp.com",
   databaseURL: "https://devoirs-pse-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "devoirs-pse",
   storageBucket: "devoirs-pse.appspot.com",
   messagingSenderId: "614730413904",
-  appId: "TON_APP_ID"
+  appId: "1:614730413904:web:a5dd478af5de30f6bede55"
 };
